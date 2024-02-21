c6b97efe570c809b479e29bf4788ba1d89db7fc0
Trevor Christian <tchristian@alaska.edu>
2024-02-20 23:28:43 -0900

    set up express

---
+++ 372site/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const router = express.Router();
+const path = require("path");
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/login.html"));
+});
+
+app.listen(8080, () => {
+    console.log("App is starting at port ", 8080);
+});
